0640776a422f169c4aacf4ba2e4d4979725d3734
Gustavo Rossatto <rossatto.gus@gmail.com>
2025-03-20 10:45:41 -0300

    Criação da page de login para reutilizar código e os testes de login válido e inválido

---
+++ tests/test-login.spec.js
@@ -0,0 +1,20 @@
+const {test, expect } = require('@playwright/test');
+const LoginPage = require('../pages/loginPage');
+
+let loginPage;
+
+test.beforeEach(async ({ page }) => {
+    await page.goto('');
+    loginPage = new LoginPage(page);
+});
+
+test('Test Valid Login', async ({ page }) => {
+    await loginPage.login('standard_user', 'secret_sauce');
+    await expect(page).toHaveURL(/inventory.html/);
+});
+
+test('Test Invalid Login', async ({ page }) => {
+    await loginPage.login('login_false', 'senha_false');
+    await page.locator('[data-test="login-button"]').click();
+    await expect(loginPage.errorMessage).toBeVisible();   
+});
